47b5bef33fe5e259bf013c984e1458ba235b9d09
francis <clyderitchie@yahoo.com>
2023-10-09 18:26:10 -0700

    Got the customer search btn to work and show a list when clicked on

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,8 @@
 // Global variables
 let toggleBtn = document.querySelector('#toggle-btn');
-
-
+let toggleList = document.querySelector('#toggle-list');
+let dropdownBtn = document.querySelector('#dropdown-btn')
+let dropdownMenu = document.querySelector('#dropdown-menu');
 
 
 
@@ -10,9 +11,14 @@ let toggleBtn = document.querySelector('#toggle-btn');
 // Header section
 toggleBtn.addEventListener('click', function(event){
     event.preventDefault();
-    let toggleList = document.querySelector('#toggle-list')
-    toggleList.innerHTML = `
-    <li>Profile</li>
-    <li>Settings</li>
-    <li>FAQ</li>`
+    toggleList.classList.toggle('show');
+    console.log('hello header btn')
+})
+
+
+// Customer search section
+dropdownBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    dropdownMenu.classList.toggle('show');
+    console.log('hello customer search btn')
 })
